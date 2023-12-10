2765bfaf1525e2db4e0b57e32657fda9492604cc
cacauisadog <cbcaue@protonmail.com>
2023-12-10 12:24:32 -0300

    add a human friendly "time it took to run" message

--- ntropy/ntropy.py
+++ ntropy/ntropy.py
@@ -14,7 +14,7 @@ def measure(func: Callable = None, *, disable_gc=False):
     @wraps(func)
     def wrapper(*args, **kwargs):
         if disable_gc:
-            sys.stdout.write("Disabling garbage collection...\n")
+            sys.stdout.write("Disabling garbage collection...\n\n")
             gc.disable()
 
         start = time.perf_counter_ns()
@@ -23,11 +23,62 @@ def measure(func: Callable = None, *, disable_gc=False):
             return func(*args, **kwargs)
         finally:
             end = time.perf_counter_ns()
-            elapsed_time_ns = end - start
-            sys.stdout.write(f"{name} function took {str(elapsed_time_ns)} ns to run")
-            sys.stdout.write("\n")
+            abs_elapsed_time_ns = end - start
+            abs_elapsed_time_ms = abs_elapsed_time_ns / 1e6
+            time_dict = _build_time_dict(abs_elapsed_time_ms)
+            pretty_message: str = _build_time_message(name, time_dict)
+
+            sys.stdout.write(pretty_message)
+            sys.stdout.write("\n\n")
             if disable_gc:
                 sys.stdout.write("Re-enabling garbage collection...\n")
                 gc.enable()
 
     return wrapper
+
+
+def _build_time_dict(abs_elapsed_time_ms):
+    miliseconds = int(abs_elapsed_time_ms % 1000)
+
+    abs_elapsed_time_sec = abs_elapsed_time_ms // 1e3
+    seconds = int(abs_elapsed_time_sec % 60)
+
+    abs_elapsed_time_min = abs_elapsed_time_sec // 60
+    minutes = int(abs_elapsed_time_min % 60)
+
+    hours = int(abs_elapsed_time_min // 60)
+
+    return {
+        "miliseconds": miliseconds,
+        "seconds": seconds,
+        "minutes": minutes,
+        "hours": hours,
+    }
+
+
+def _build_time_message(func_name, time_dict):
+    message = f"The function '{func_name}' took"
+
+    if time_dict["hours"] == 1:
+        message += " 1 hour"
+    if time_dict["hours"] > 1:
+        message += f" {time_dict['hours']} hours"
+
+    if time_dict["minutes"] == 1:
+        message += " 1 minute"
+    if time_dict["minutes"] > 1:
+        message += f" {time_dict['minutes']} minutes"
+
+    if time_dict['seconds'] == 1:
+        message += ' 1 seconds'
+    if time_dict['seconds'] > 1:
+        message += f' {time_dict['seconds']} seconds'
+
+    if time_dict['miliseconds'] == 1:
+        message += ' 1 milisecond'
+    if time_dict['miliseconds'] > 1:
+        message += f' {time_dict['miliseconds']} miliseconds'
+
+    message += ' to run.'
+
+    return message
